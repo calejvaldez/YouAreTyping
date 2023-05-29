0d7f46f33581c0e97b232639576800254d2b1a86
Carlos Valdez <cvaldezh@uci.edu>
2023-05-28 23:42:35 -0700

    feat: started TS development

---
+++ src/cvaldez/urtyping/static/js/3_typing.ts
@@ -0,0 +1,14 @@
+interface Message {
+    id: string
+    user_id: string
+    content: string
+    timestamp: string
+    from: string
+    to: string
+}
+
+interface User {
+    id: string
+    username: string
+}
+
