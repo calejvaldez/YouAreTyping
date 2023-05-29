cd42395683b42764931a3cb68396cfc48eddf8e7
Carlos Valdez <cvaldezh@uci.edu>
2023-05-28 22:02:38 -0700

    feat: html started

--- src/cvaldez/urtyping/views.py
+++ src/cvaldez/urtyping/views.py
@@ -3,9 +3,13 @@
 #
 # The views for You Are Typing.
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('You Are Typing', __name__,
                template_folder='templates',
                static_folder='static',
                url_prefix='/typing/')
+
+@bp.route('/text/')
+def text():
+    return render_template('3_typing.html')
