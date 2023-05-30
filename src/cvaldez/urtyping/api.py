2f24d7c24f6124a28cd68813eaeaafee1bc49463
Carlos Valdez <cvaldezh@uci.edu>
2023-05-29 23:10:07 -0700

    feat: get_messages function created

--- src/cvaldez/urtyping/api.py
+++ src/cvaldez/urtyping/api.py
@@ -3,9 +3,9 @@
 #
 # Source code for the You Are Typing API.
 
-from flask import Blueprint, request
+from flask import Blueprint, request, Response
 import json
-from tools import fetch_messages, UserExistsError, UserNotFoundError
+from tools import fetch_messages, UserExistsError, UserNotFoundError, get_uuid_by_token
 
 bp = Blueprint('You Are Typing API', __name__,
                template_folder='templates',
@@ -15,7 +15,19 @@ bp = Blueprint('You Are Typing API', __name__,
 
 @bp.route('/get-messages/', methods=['GET'])
 def get_messages():
-    ...
+    user_id = get_uuid_by_token(request.headers['Bearer'])
+
+    if not user_id:
+        return Response({'ERROR': 'Unauthorized.'}, status=401)
+
+    return Response(json.dumps({'data': [{
+        'id': x.id,
+        'user_id': x.user_id,
+        'content': x.content,
+        'timestamp': x.timestamp,
+        'from': x.message_from,
+        'to': x.to
+    } for x in fetch_messages(user_id)]}), status=200)
 
 
 @bp.route("/send-message/", methods=['POST'])
