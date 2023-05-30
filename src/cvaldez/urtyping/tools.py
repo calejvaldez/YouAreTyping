5542c33ae4143631a5500735bcf9d516a1d0f128
Carlos Valdez <cvaldezh@uci.edu>
2023-05-29 21:31:02 -0700

    feat: mostly finished Message class
    I just need to add the encryption features.

--- src/cvaldez/urtyping/tools.py
+++ src/cvaldez/urtyping/tools.py
@@ -6,6 +6,7 @@ import time
 import psycopg2
 from cryptography.fernet import Fernet
 import os
+import uuid
 
 
 class UserExistsError(Exception):
@@ -57,18 +58,34 @@ class User:
 
 
 class Message:
-    def __init__(self, data: dict):
-        self.id: str
-        self.user_id: str
-        self.content: str
-        self.timestamp: str
-        self.message_from: str
-        self.to: str
+    def __init__(self, data: tuple):
+        self.id = data[0]
+        self.user_id = data[1]
+        self.content = data[2] # TODO: unencrypt
+        self.timestamp = data[3]
+        self.message_from = data[4]
+        self.to = data[5]
 
     @staticmethod
-    def new(content: str, *, sender: str, uuid: str) -> 'Message':
-        ...
+    def new(content: str, *, sender: str, user_id: str) -> 'Message':
+        u = User(user_id)
+        content = content # TODO: encrypt
+
+        if u.username == sender:
+            send_to = 'friend'
+        else:
+            send_to = u.username
+
+        with psycopg2.connect(os.getenv("DB_LINK")) as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO 3_messages(id, user_id, content, timestamp, from, to) VALUES(%s, %s, %s, %s, %s, %s)", (uuid.uuid4(), user_id, content, time.time(), sender, send_to))
 
 
-def fetch_messages(amount=30) -> list[Message]:
-    ...
+def fetch_messages(user_id, amount=30) -> list[Message]:
+    with psycopg2.connect(os.getenv("DB_LINK")) as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM 3_messages WHERE user_id=%s", (user_id,))
+
+        fetched = cur.fetchmany(amount)
+
+        return [Message(x) for x in fetched]
